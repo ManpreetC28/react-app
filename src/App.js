742fbcd55d95f19ed9f3d169c4462a8ed9f37276
ManpreetC28 <manpreetcheema282000@gmail.com>
2023-02-22 17:30:51 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import About from './components/About';
-import Alerts from './components/Alerts';
-
-
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import Alerts from "./components/Alerts";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-  <>
-    <Navbar title="TextUtils" aboutus="About us"/>
-    <Alerts Alerts="This is"/>
-    <div className="container my-3">
-    <About/>
-    <TextForm title="Text"/>
-
-    </div>
-  </>
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutus="About us" />
+        <Alerts Alerts={alert} />
+        <div className="container my-3">
+          <Switch>
+            <Route exact path="/about">
+              <About/>
+            </Route>
+            <Route exact path="/">
+              <TextForm title="Text" />
+            </Route>
+          </Switch>
+        
+        </div>
+      </Router>
+    </>
   );
 }
 
--- src/components/About.js
+++ src/components/About.js
@@ -26,7 +26,7 @@ export default function About() {
     }
   return (
     <div className="container" style={myStyle}>
-    <h1 className="my-3">About Us</h1>
+    <h1 className="my-6">About Us</h1>
       <div className="accordion" id="accordionExample">
         <div className="accordion-item">
           <h2 className="accordion-header" id="headingOne">
